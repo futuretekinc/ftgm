eadc74357bb53177b7cdf3fc4b7efe471d2eef83
Inhyun Cho <xtra72@gmail.com>
2016-05-01 23:34:54 +0900

    .

---
+++ ftm/ftm_util.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "ftm_util.h"
+
+FTM_RET	FTM_makeID
+(
+	FTM_CHAR_PTR	pID,
+	FTM_ULONG		ulLen
+)
+{
+	ASSERT(pID != NULL);
+	FTM_RET			xRet;
+	struct timeval	xTime;
+	FTM_CHAR		pNewID[FTM_ID_LEN+1];
+	FTM_ULONG		pValue[4];
+
+	gettimeofday(&xTime, NULL);
+	pValue[0] = (FTM_ULONG)xTime.tv_sec;
+	pValue[1] = (FTM_ULONG)(xTime.tv_usec << 16) + (FTM_ULONG)xTime.tv_usec;
+	pValue[2] = (FTM_ULONG)pValue[0] ^ 0xFFFFFFFF;
+	pValue[3] = (FTM_ULONG)pValue[1] ^ 0xFFFFFFFF;
+
+	sprintf(pNewID, "%08lx%08lx%08lx%08lx", pValue[0], pValue[1], pValue[2], pValue[3]);
+
+	strncpy(pID, pNewID, ulLen);
+
+	return	FTM_RET_OK;
+}
---
+++ ftm/ftm_util.h
@@ -0,0 +1,13 @@
+#ifndef	_FTM_UTIL_H_
+#define	_FTM_UTIL_H_
+
+#include <time.h>
+#include "ftm.h"
+
+FTM_RET	FTM_makeID
+(
+	FTM_CHAR_PTR	pID,
+	FTM_ULONG		ulLen
+);
+
+#endif
